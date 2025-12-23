13db0065c7b1a8695392a6f3f213bf907e3bedc9
snehahalnor107 <snehahalnor107@gmail.com>
2025-12-23 20:56:41 +0530

    Created Spring boot project and also created packeges and add dependancy

---
+++ StudentManagmentSystemPratice/src/main/java/edu/cjc/sms/app/StudentManagmentSystemPraticeApplication.java
@@ -0,0 +1,13 @@
+package edu.cjc.sms.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagmentSystemPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagmentSystemPraticeApplication.class, args);
+	}
+
+}
---
+++ StudentManagmentSystemPratice/src/test/java/edu/cjc/sms/app/StudentManagmentSystemPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cjc.sms.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagmentSystemPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
